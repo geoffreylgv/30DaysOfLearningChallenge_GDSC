327e7ded4b1b2db23f2a920f710a71c6510e21ab
geoffreylgv <evanomarkez@gmail.com>
2022-10-15 18:14:47 +0000

    Day6 project created

---
+++ UniversitiesAPI/src/main/java/com/github/geoffreylgv/universitiesapi/UniversityApp.java
@@ -0,0 +1,16 @@
+package com.github.geoffreylgv.universitiesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author geoffreylgv
+ */
+@SpringBootApplication
+public class UniversityApp {
+
+    public static void main(String[] var) {
+        SpringApplication.run(UniversityApp.class, var);
+    }
+}
