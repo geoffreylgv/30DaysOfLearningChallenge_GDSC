5c3f3f942c89d83759f9f88a0a5f0def7568a7b0
geoffreylgv <evanomarkez@gmail.com>
2022-10-16 19:38:27 +0000

    type univ

---
+++ UniversitiesAPI/src/main/java/com/github/geoffreylgv/universitiesapi/utils/enumation/TypeUniv.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.github.geoffreylgv.universitiesapi.utils.enumation;
+
+/**
+ *
+ * @author geoffreylgv
+ */
+public enum TypeUniv {
+    PB("Public"), PR("Private"), IS("Inter State");
+    private String libelle;
+
+    public String getLibelle() {
+        return libelle;
+    }
+
+    private TypeUniv(String libelle) {
+        this.libelle = libelle;
+    }
+
+    public static TypeUniv fromValue(String str) {
+        for (TypeUniv c : TypeUniv.values()) {
+            if (c.name().equals(str)) {
+                return c;
+            }
+        }
+        return null;
+    }
+}
